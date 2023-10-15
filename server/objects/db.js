de0dacc318bc7e2f2786e23f4321effdbf43e2b9
viettungvuong <viettungvuong@gmail.com>
2023-10-15 08:39:49 +0700

    Thêm db

---
+++ server/objects/db.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: process.env.user,
+  password: process.env.password,
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
