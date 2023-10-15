7be24c3be03111e8540e177946ff459ff483db5d
viettungvuong <viettungvuong@gmail.com>
2023-10-15 21:45:46 +0700

    Database

--- server/objects/db.js
+++ server/objects/db.js
@@ -10,4 +10,16 @@ var con = mysql.createConnection({
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected to MySQL!");
+
+  con.query("create database if not exists main", function (err, result) {
+    if (err) throw err;
+    console.log("Database created"); //create database
+
+    con.query("create table if not exists users", function (err, result) {
+      if (err) throw err;
+      console.log("Table users created"); //create users database
+    });
+  });
 });
+
+exports.createDb = () => {};
