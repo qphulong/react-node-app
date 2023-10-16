6f4c13bc4553928f5f7784b1d842001ec7ac5072
viettungvuong <viettungvuong@gmail.com>
2023-10-16 16:50:58 +0700

    Add function to Write to MongoDB

--- server/objects/db.js
+++ server/objects/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.DB_URI;
+const uri = process.env.DB_URI; //lấy từ file uri
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -13,15 +13,22 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  //connect to mongodb
   try {
     await client.connect();
     await client.db("Tung").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Connected to mongoDB");
   } finally {
     await client.close();
   }
+}
+
+async function addDoc(collectionName, docs) {
+  //add documents to a collection
+  const myDB = client.db("Tung");
+  const myColl = myDB.collection(collectionName);
+
+  const insertManyresult = await myColl.insertMany(docs); //add documents
 }
 
 module.exports = {
