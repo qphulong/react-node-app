78c59c697ea2f6cba223898ddf58f451945b8756
viettungvuong <viettungvuong@gmail.com>
2023-10-25 19:55:07 +0700

    Change password

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,4 +4,11 @@ exports.signIn = (req, res) => {};
 
 exports.signUp = (req, res) => {};
 
-exports.changePassword = (req, res) => {};
+exports.changePassword = (req, res) => {
+  const userId = req.body.userId;
+  const newPassword = req.body.newPassword;
+
+  const confirmPassword = req.body.confirmPassword;
+
+  userFunctions.changePassword(userId, newPassword, confirmPassword);
+};
--- server/functions/user.js
+++ server/functions/user.js
@@ -1,5 +1,17 @@
 const User = require("../models/user");
 
-async function changePassword(userId, newPassword) {
-  await User.updateOne({ userId: userId }, { password: newPassword });
+async function changePassword(userId, newPassword, confirmPassword) {
+  const existingPassword = await getPasswordByUserId(userId); //retrieve the current password
+
+  if (existingPassword) {
+    if (existingPassword == confirmPassword) {
+      //check if the existing password is the same as the confirm password
+      await User.updateOne({ userId: userId }, { password: newPassword });
+      console.log("Password updated successfully!");
+    } else {
+      console.log("Password doesn't match with the current one.");
+    }
+  } else {
+    console.log("User not found with the given userId.");
+  }
 }
