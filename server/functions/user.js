76ee30a62f23103955390ca5034d1907a75d4715
viettungvuong <viettungvuong@gmail.com>
2023-10-27 21:34:07 +0700

    Add friend function

--- server/functions/user.js
+++ server/functions/user.js
@@ -22,7 +22,7 @@ async function changePassword(userId, newPassword, confirmPassword) {
   if (isPasswordValid) {
     //check if the existing password is the same as the confirm password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    const hashedPassword = await bcrypt.hash(newPassword, salt); //hash password next
 
     await User.updateOne({ userId: userId }, { password: hashedPassword });
     console.log("Password updated successfully!");
@@ -31,7 +31,34 @@ async function changePassword(userId, newPassword, confirmPassword) {
   }
 }
 
+async function addFriend(userId, friendId) {
+  if (userId === friendId) {
+    console.log("Cannot add the same friend");
+    return;
+  }
+
+  const user = await User.findOne({ userId: userId }); //find user
+
+  //find friend
+  User.findOne({ userId: friendId }, (err, userB) => {
+    if (err) {
+      console.log("Error when adding friend");
+    } else {
+      user.friends.push(userB._id);
+
+      user.save((err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("New friend added");
+        }
+      });
+    }
+  });
+}
+
 module.exports = {
   signIn,
   changePassword,
+  addFriend,
 };
