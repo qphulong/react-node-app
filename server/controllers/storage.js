2cddddc2eb69f8af58b1177d254f0a69492a16cd
viettungvuong <viettungvuong@gmail.com>
2023-11-01 15:20:57 +0700

    Add library

--- server/controllers/storage.js
+++ server/controllers/storage.js
@@ -28,7 +28,8 @@ exports.uploadFile = (req, res) => {
 exports.retrieveImages = (req, res) => {
   const postId = req.postId;
 
-  getDownloadUrls(postId)
+  storageFunctions
+    .getDownloadUrls(postId)
     .then((downloadUrls) => {
       console.log("Download URLs:", downloadUrls);
     })
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/user");
+const { query } = require("express-validator");
 
 const router = express.Router();
 
