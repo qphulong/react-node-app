bcaff0013346232da3980ec7f40a2c2cc9f4963f
ThanhPhuc18 <vtphuc21@apcs.fitus.edu.vn>
2023-11-06 14:43:53 +0700

    Update storage.js

--- server/controllers/storage.js
+++ server/controllers/storage.js
@@ -1,7 +1,28 @@
 const storage = require("../firebase/storage");
 const storageFunctions = require("../functions/storage");
+const express = require('express')
+const multer = require('multer')
+const firebase = require('./firebase')
+const app = express()
 
-exports.uploadFile = (req, res) => {};
+exports.uploadFile = (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("Error: No files found");
+  }
+  const blob = storage.bucket.file(req.file.originalname);
+  const blobWriter = blob.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype,
+    },
+  });
+  blobWriter.on("error", (err) => {
+    console.log(err); //error uploading file
+  });
+  blobWriter.on("finish", () => {
+    res.status(200).send("File uploaded.");
+  });
+  blobWriter.end(req.file.buffer);
+};
 
 exports.retrieveImages = (req, res) => {
   const postId = req.postId;
