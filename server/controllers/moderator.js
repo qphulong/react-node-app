0337067d9e68e53caee537714213821015582a7b
qplong21 <qphulong@gmail.com>
2023-11-08 14:20:10 +0700

    controller/moderator.js
    tôi ko chắc tôi làm đúng ko, ngài Tùng
    
    Co-Authored-By: viettungvuong <13688556+viettungvuong@users.noreply.github.com>

--- server/controllers/moderator.js
+++ server/controllers/moderator.js
@@ -1,1 +1,24 @@
 const { deletePost } = require("../functions/post");
+const moderatorFunctions = require("../functions/moderator");
+const Moderator = require("../models/moderator");
+
+exports.signIn = (req, res) => {
+    const moderatorId = req.body.moderatorId;
+    const password = req.body.password;
+
+    moderatorFunctions.signIn(moderatorId, password);
+};
+
+exports.changePassword = (req, res) => {
+    const moderatorId = req.body.moderatorIdId;
+    const newPassword = req.body.newPassword;
+
+    const confirmPassword = req.body.confirmPassword;
+
+    moderatorFunctions.changePassword(moderatorId, newPassword, confirmPassword);
+};
+
+exports.deletePost = (req, res) => {
+    const postId = req.body.postId;
+    moderatorFunctions.removePost(postId);
+};
