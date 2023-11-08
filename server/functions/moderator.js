e727df4addf4ad812a8d38d00a545f361f9ad6d8
qplong21 <qphulong@gmail.com>
2023-11-08 10:27:29 +0700

    removePost()
    logic not yet implemented

--- server/functions/moderator.js
+++ server/functions/moderator.js
@@ -1,62 +1,75 @@
 const Moderator = require("../models/moderator");
+const Post = require("../models/post");
 const bcrypt = require("bcrypt");
 
 async function signIn(moderatorId, inputPassword) {
-  try {
-    const moderator = await Moderator.findOne({ moderatorId: moderatorId });
+    try {
+        const moderator = await Moderator.findOne({ moderatorId: moderatorId });
 
-    if (!moderator) {
-      throw new Error("Moderator not found");
+        if (!moderator) {
+            throw new Error("Moderator not found");
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            inputPassword,
+            moderator.password
+        );
+
+        if (isPasswordValid) {
+            console.log("Login successfully!");
+        } else {
+            throw new Error("Password doesn't match");
+        }
+    } catch (error) {
+        console.error("Error signing in:", error.message);
     }
-
-    const isPasswordValid = await bcrypt.compare(
-      inputPassword,
-      moderator.password
-    );
-
-    if (isPasswordValid) {
-      console.log("Login successfully!");
-    } else {
-      throw new Error("Password doesn't match");
-    }
-  } catch (error) {
-    console.error("Error signing in:", error.message);
-  }
 }
 
 async function changePassword(moderatorId, newPassword, confirmPassword) {
-  try {
-    const moderator = await Moderator.findOne({ moderatorId: moderatorId });
+    try {
+        const moderator = await Moderator.findOne({ moderatorId: moderatorId });
 
-    if (!moderator) {
-      throw new Error("Moderator not found");
+        if (!moderator) {
+            throw new Error("Moderator not found");
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            confirmPassword,
+            moderator.password
+        );
+
+        if (isPasswordValid) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+            await Moderator.updateOne({ moderatorId: moderatorId }, { password: hashedPassword });
+            console.log("Password updated successfully!");
+        } else {
+            throw new Error("Password doesn't match");
+        }
+    } catch (error) {
+        console.error("Error changing password:", error.message);
     }
-
-    const isPasswordValid = await bcrypt.compare(
-      confirmPassword,
-      moderator.password
-    );
-
-    if (isPasswordValid) {
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(newPassword, salt);
-
-      await Moderator.updateOne(
-        { moderatorId: moderatorId },
-        { password: hashedPassword }
-      );
-      console.log("Password updated successfully!");
-    } else {
-      throw new Error("Password doesn't match");
-    }
-  } catch (error) {
-    console.error("Error changing password:", error.message);
-  }
 }
 
-//ngài làm thêm mấy cái xoá bài (nếu vi phạm) và giữ bài (nếu bài không vi phạm)
+async function removePost(postId) {
+    try {
+        const post = await Post.findById(postId);
+        if (!post) {
+            throw new Error(`Post with ID ${postId} not found`);
+        }
+
+        // Implement logic to handle removal of the post here
+
+
+        console.log(`Post with ID ${postId} removed successfully.`);
+    } catch (error) {
+        console.error("Error removing post:", error.message);
+    }
+}
 
 module.exports = {
-  signIn,
-  changePassword,
+    signIn,
+    changePassword,
+    removePost
 };
