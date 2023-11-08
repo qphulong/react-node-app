b520ad8d5ed5bfe308db5db552355b1c207df60d
viettungvuong <viettungvuong@gmail.com>
2023-11-08 09:05:23 +0700

    Content moderator note

--- server/functions/moderator.js
+++ server/functions/moderator.js
@@ -2,50 +2,61 @@ const Moderator = require("../models/moderator");
 const bcrypt = require("bcrypt");
 
 async function signIn(moderatorId, inputPassword) {
-    try {
-        const moderator = await Moderator.findOne({ moderatorId: moderatorId });
+  try {
+    const moderator = await Moderator.findOne({ moderatorId: moderatorId });
 
-        if (!moderator) {
-            throw new Error("Moderator not found");
-        }
-
-        const isPasswordValid = await bcrypt.compare(inputPassword, moderator.password);
-
-        if (isPasswordValid) {
-            console.log("Login successfully!");
-        } else {
-            throw new Error("Password doesn't match");
-        }
-    } catch (error) {
-        console.error("Error signing in:", error.message);
+    if (!moderator) {
+      throw new Error("Moderator not found");
     }
+
+    const isPasswordValid = await bcrypt.compare(
+      inputPassword,
+      moderator.password
+    );
+
+    if (isPasswordValid) {
+      console.log("Login successfully!");
+    } else {
+      throw new Error("Password doesn't match");
+    }
+  } catch (error) {
+    console.error("Error signing in:", error.message);
+  }
 }
 
 async function changePassword(moderatorId, newPassword, confirmPassword) {
-    try {
-        const moderator = await Moderator.findOne({ moderatorId: moderatorId });
+  try {
+    const moderator = await Moderator.findOne({ moderatorId: moderatorId });
 
-        if (!moderator) {
-            throw new Error("Moderator not found");
-        }
-
-        const isPasswordValid = await bcrypt.compare(confirmPassword, moderator.password);
-
-        if (isPasswordValid) {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(newPassword, salt);
-
-            await Moderator.updateOne({ moderatorId: moderatorId }, { password: hashedPassword });
-            console.log("Password updated successfully!");
-        } else {
-            throw new Error("Password doesn't match");
-        }
-    } catch (error) {
-        console.error("Error changing password:", error.message);
+    if (!moderator) {
+      throw new Error("Moderator not found");
     }
+
+    const isPasswordValid = await bcrypt.compare(
+      confirmPassword,
+      moderator.password
+    );
+
+    if (isPasswordValid) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+      await Moderator.updateOne(
+        { moderatorId: moderatorId },
+        { password: hashedPassword }
+      );
+      console.log("Password updated successfully!");
+    } else {
+      throw new Error("Password doesn't match");
+    }
+  } catch (error) {
+    console.error("Error changing password:", error.message);
+  }
 }
 
+//ngài làm thêm mấy cái xoá bài (nếu vi phạm) và giữ bài (nếu bài không vi phạm)
+
 module.exports = {
-    signIn,
-    changePassword,
+  signIn,
+  changePassword,
 };
--- server/models/moderator.js
+++ server/models/moderator.js
@@ -2,37 +2,34 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const moderatorSchema = new mongoose.Schema({
-    moderatorId: {
-        type: String,
-        required: true,
-    },
+  moderatorId: {
+    type: String,
+    required: true,
+  },
 
-    profilePic: {
-        type: String, // to store url of profile picture
-    },
+  //content moderator doesn't need a profile pic
 
-    password: {
-        type: String,
-        required: true,
-    },
-
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-moderatorSchema.pre("save", async function(next) {
-    try {
-        // hash password if only editing / new moderator
-        if (!this.isModified("password")) {
-            return next();
-        }
-
-        //hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        return next(error);
+moderatorSchema.pre("save", async function (next) {
+  try {
+    // hash password if only editing / new moderator
+    if (!this.isModified("password")) {
+      return next();
     }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 module.exports = mongoose.model("Moderator", moderatorSchema);
