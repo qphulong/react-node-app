3781730a2aae316426028e5885e2118de1bd3deb
Tran Minh Khanh <kidopro03@gmail.com>
2023-11-08 22:15:34 +0700

    Register Page UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import Login from "./pages/login/login";
+import Register from "./pages/register/register";
 
 function App() {
   return (
       <div>
-          <Login/>
+          <Register/>
       </div>
   );
 }
--- client/src/pages/register/register.js
+++ client/src/pages/register/register.js
@@ -3,7 +3,26 @@ import './register.scss'
 function Register(){
     return (
         <div className = "register">
-
+            <div className = "card">
+                <div className = "left">
+                    <h1>OnlyMe</h1>
+                    <p>
+                        Share unforgetable memories and daily life stories only for your families and friends here.
+                    </p>
+                    <span>Do you have an account?</span>
+                    <button>Login</button>
+                </div>
+                <div className = "right">
+                    <h1>Register</h1>
+                    <form>
+                        <input type = 'text' placeholder = 'Username'/>
+                        <input type = 'email' placeholder = 'Email'/>
+                        <input type = 'password' placeholder = 'Password'/>
+                        <input type = 'text' placeholder = 'Name'/>
+                        <button>Register</button>
+                    </form> 
+                </div>
+            </div>
         </div>
     )
 }
