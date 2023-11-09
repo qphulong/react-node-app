77b800a8e6ab45a75ff1578f135c05ac881fc7ca
viettungvuong <viettungvuong@gmail.com>
2023-11-09 16:14:37 +0700

    Validate text changed number of words

--- server/routes/storage.js
+++ server/routes/storage.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/upload", storageController.uploadImage);
+router.post("/upload", storageController.uploadImages);
 
 router.post("/download", storageController.retrieveImages);
 
