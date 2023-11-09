a48e9d76e4deb490466822da47d754465f49dc75
Tran Minh Khanh <kidopro03@gmail.com>
2023-11-09 11:17:32 +0700

    Leftbar UI  + divide Flex for outlet

--- client/src/components/leftbar/leftBar.js
+++ client/src/components/leftbar/leftBar.js
@@ -1,4 +1,17 @@
 import './leftBar.scss'
+import Friends from "../../assets/1.png"
+import Group from "../../assets/2.png"
+import Market from "../../assets/3.png"
+import Watch from "../../assets/4.png"
+import Memories from "../../assets/5.png"
+import Events from "../../assets/6.png"
+import Gaming from "../../assets/7.png"
+import Galleries from "../../assets/8.png"
+import Video from "../../assets/9.png"
+import Messages from "../../assets/10.png"
+import Tutorials from "../../assets/11.png"
+import Courses from "../../assets/12.png"
+import Funds from "../../assets/13.png"
 
 function LeftBar(){
     return (
@@ -12,8 +25,78 @@ function LeftBar(){
                     </div>
 
                     <div className='item'>
-                        <img src='' alt=''/>
+                        <img src={Friends} alt=''/>
                         <span>Friends</span>
+                    </div>
+
+                    <div className='item'>
+                        <img src={Group} alt=''/>
+                        <span>Group</span>
+                    </div>
+
+                    <div className='item'>
+                        <img src={Market} alt=''/>
+                        <span>Marketplace</span>
+                    </div>
+
+                    <div className='item'>
+                        <img src={Watch} alt=''/>
+                        <span>Watch</span>
+                    </div>
+
+                    <div className='item'>
+                        <img src={Memories} alt=''/>
+                        <span>Memories</span>
+                    </div>
+
+                    <hr/>
+                    <div className='menu'>
+                        <span>Shortcuts</span>
+
+                        <div className='item'>
+                            <img src={Events} alt=''/>
+                            <span>Events</span>
+                        </div>
+
+                        <div className='item'>
+                            <img src={Gaming} alt=''/>
+                            <span>Gaming</span>
+                        </div>
+
+                        <div className='item'>
+                            <img src={Galleries} alt=''/>
+                            <span>Galleries</span>
+                        </div>
+
+                        <div className='item'>
+                            <img src={Video} alt=''/>
+                            <span>Video</span>
+                        </div>
+
+                        <div className='item'>
+                            <img src={Messages} alt=''/>
+                            <span>Messages</span>
+                        </div>
+                    </div>
+
+                    <hr/>
+                    <div className='menu'>
+
+                        <span>Others</span>
+                        <div className='item'>
+                            <img src={Tutorials} alt=''/>
+                            <span>Tutorials</span>
+                        </div>
+
+                        <div className='item'>
+                            <img src={Courses} alt=''/>
+                            <span>Courses</span>
+                        </div>
+
+                        <div className='item'>
+                            <img src={Funds} alt=''/>
+                            <span>Funds</span>
+                        </div>
                     </div>
                 </div>
             </div>
