e95c7612e92dcf76a85c86e088768079d2fa0c09
Tran Minh Khanh <kidopro03@gmail.com>
2023-11-09 10:37:06 +0700

    Navbar UI + Assets LeftBar UI + Basic layout LeftBar

--- client/src/components/leftbar/leftBar.js
+++ client/src/components/leftbar/leftBar.js
@@ -3,7 +3,20 @@ import './leftBar.scss'
 function LeftBar(){
     return (
         <div className = "leftbar">
-            Leftbar
+            <div className='container'>
+                <div className='menu'>
+                    <div className='user'>
+                        <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                        alt=''/>
+                        <span>TMK</span>
+                    </div>
+
+                    <div className='item'>
+                        <img src='' alt=''/>
+                        <span>Friends</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/navbar/navBar.js
+++ client/src/components/navbar/navBar.js
@@ -1,9 +1,39 @@
 import './navBar.scss'
-
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Link } from 'react-router-dom';
 function NavBar(){
     return (
         <div className = "navbar">
-            Navbar
+            <div className='left'>
+                <Link to='/' style = {{textDecoration: 'none'}}>
+                    <span>OnlyMe</span>
+                </Link>
+                <HomeOutlinedIcon/>
+                <DarkModeOutlinedIcon/>
+                <GridViewOutlinedIcon/>
+                <div className='search'>
+                    <SearchOutlinedIcon/>
+                    <input type='text' placeholder='Search...'/>
+                </div>
+            </div>
+
+            <div className='right'>
+                <PersonOutlinedIcon/>
+                <EmailOutlinedIcon/>
+                <NotificationsOutlinedIcon/>
+                <div className='user'>
+                    <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                    alt=''/>
+                    <span>TMK</span>
+                </div>
+            </div>
         </div>
     )
 }
