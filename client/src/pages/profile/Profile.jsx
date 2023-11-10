dcb8e0674af8542eafa13958a625d32e126ccc3a
NhutThi2404 <nhutth2404@gmail.com>
2023-11-10 10:30:07 +0700

    basic interface for profile

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,6 +3,10 @@ import FacebookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import PlaceIcon from '@mui/icons-material/Place';
+import LanguageIcon from '@mui/icons-material/Language';
+import EmailIcon from '@mui/icons-material/Email';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 
 export default function () {
@@ -28,8 +32,26 @@ export default function () {
                 <LinkedInIcon fontSize="large"/>
               </a>
             </div>
-            <div className="center"></div>
-            <div className="right"></div>
+            <div className="center">
+              <span>Username</span>
+              <div className="info">
+                <div className="item">
+                  <PlaceIcon fontSize="large"/>
+                  <span>VietNam</span>
+                </div>
+                <div className="item">
+                  <LanguageIcon fontSize="large"/>
+                  <span>VietNam</span>
+                </div>
+                <button>
+                  follow
+                </button>
+              </div>
+            </div>
+            <div className="right">
+              <EmailIcon/>
+              <MoreVertIcon/>
+            </div>
           </div>
 
         </div>
