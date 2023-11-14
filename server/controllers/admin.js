bf8f4b86b3b1e53e07558534968dea26fd93f839
qplong21 <qphulong@gmail.com>
2023-11-14 23:27:53 +0700

    unassignModerator

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -4,3 +4,8 @@ exports.assignModerator = (req, res) => {
     const userId = req.body.userId;
     adminFunctions.assignModerator(userId);
 };
+
+exports.unassignModerator = (req, res) => {
+    const userId = req.body.userId;
+    adminFunctions.unassignModerator(userId);
+};
--- server/functions/admin.js
+++ server/functions/admin.js
@@ -25,6 +25,30 @@ async function assignModerator(userId) {
     }
 }
 
+async function unassignModerator(userId) {
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        if (!currentUser.isAdmin) {
+            throw new Error("Unauthorized: Only admins can assign moderator roles");
+        }
+
+        user.isModerator = false;
+
+        await user.save();
+
+        return { success: true, message: "User assigned as a moderator successfully" };
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: error.message || "Error assigning moderator role" };
+    }
+}
+
 module.exports = {
     assignModerator,
+    unassignModerator,
 };
