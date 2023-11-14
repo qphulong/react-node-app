01feaf98595b8d7d31ed4f33c219818b2f7a5148
qplong21 <qphulong@gmail.com>
2023-11-14 21:34:08 +0700

    functions/admin.js

---
+++ server/functions/admin.js
@@ -0,0 +1,11 @@
+const { currentUser } = require("../app");
+const Post = require("../models/post");
+const bcrypt = require("bcrypt");
+
+async function giveModeratorJob(userId) {
+
+}
+
+module.exports = {
+    giveModeratorJob,
+};
