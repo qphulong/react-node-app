58eac9b295a796e5fa12e3101a97a31a4cab4855
viettungvuong <viettungvuong@gmail.com>
2023-11-15 13:53:17 +0700

    Delete sign in

--- server/controllers/moderator.js
+++ server/controllers/moderator.js
@@ -5,22 +5,6 @@ const { ModeratedPostRepository } = require("../repositories/moderator");
 
 let moderatorRepo = new ModeratedPostRepository(); //use this repository
 
-exports.signIn = (req, res) => {
-  const moderatorId = req.body.moderatorId;
-  const password = req.body.password;
-
-  moderatorFunctions.signIn(moderatorId, password);
-};
-
-exports.changePassword = (req, res) => {
-  const moderatorId = req.body.moderatorIdId;
-  const newPassword = req.body.newPassword;
-
-  const confirmPassword = req.body.confirmPassword;
-
-  moderatorFunctions.changePassword(moderatorId, newPassword, confirmPassword);
-};
-
 exports.deletePost = (req, res) => {
   const postId = req.body.postId;
   moderatorFunctions.removePost(postId);
