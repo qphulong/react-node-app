48d9e187fa1eeab40f015ee2c711bbb94b4155a0
viettungvuong <viettungvuong@gmail.com>
2023-11-18 15:23:27 +0700

    Add to the images array field

--- server/controllers/storage.js
+++ server/controllers/storage.js
@@ -1,12 +1,13 @@
 const storage = require("../firebase/storage");
 const storageFunctions = require("../functions/storage");
 const multer = require("multer");
+const Post = require("../models/post");
 
 const upload = multer({
   storage: multer.memoryStorage(),
 });
 
-exports.uploadImages = (req, res) => {
+exports.uploadImages = async (req, res) => {
   if (!req.files) {
     return res.status(400).send("Error: No Image found");
   }
@@ -16,6 +17,7 @@ exports.uploadImages = (req, res) => {
 
   for (const file of files) {
     const blob = storage.bucket.file(postId + "/" + req.file.originalname);
+
     const blobWriter = blob.createWriteStream({
       metadata: {
         contentType: file.mimetype,
@@ -35,14 +37,31 @@ exports.uploadImages = (req, res) => {
 
     uploadPromises.push(
       new Promise((resolve, reject) => {
-        blobWriter.on("finish", resolve);
+        blobWriter.on("finish", () => {
+          // Get the URL of the uploaded file
+          blob
+            .getSignedUrl({
+              action: "read",
+              expires: "03-09-2491",
+            })
+            .then((signedUrls) => {
+              // signedUrls[0] contains the file's public URL
+              resolve(signedUrls[0]);
+            });
+        });
         blobWriter.on("error", reject);
       })
     );
   }
 
   Promise.all(uploadPromises)
-    .then(() => {
+    .then((imageUrls) => {
+      // imageUrls is an array of URLs of the uploaded images
+      Post.updateOne(
+        { _id: postId },
+        { $push: { images: { $each: imageUrls } } }
+      ); //add to the array field all image URLs
+
       res.status(200).send("All Images uploaded successfully.");
     })
     .catch((err) => {
