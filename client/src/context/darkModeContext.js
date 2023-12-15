1507b145b7eac2ce2ac41866c52de02128b6b394
Tran Minh Khanh <kidopro03@gmail.com>
2023-12-15 17:19:42 +0700

    Hmmmm

---
+++ client/src/context/darkModeContext.js
@@ -0,0 +1,22 @@
+import { createContext, useEffect } from "react"
+
+export const DarkModeContext = createContext()
+
+export const DarkModeContextProvider = ({children}) => {
+    const [darkMode, setDarkMode] = useState(localStorage.getItem("darkMode") || false);
+
+    const toggle = () => {
+        setDarkMode(!darkMode)
+    }
+
+    useEffect(() => {
+        localStorage.setItem('darkMode',darkMode)
+    },[darkMode])
+
+    return (
+        <DarkModeContext.Provider value = {{darkMode, toggle}}>
+            {children}
+        </DarkModeContext.Provider>
+    )
+
+}
