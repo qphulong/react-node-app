f834051a0400656ee9b3b16dcebc345b357ba7cb
Tran Minh Khanh <kidopro03@gmail.com>
2023-12-15 16:32:35 +0700

    update rightbar

--- client/src/components/rightBar/RightBar.js
+++ client/src/components/rightBar/RightBar.js
@@ -1,11 +1,127 @@
 import './rightBar.scss'
 
-const RightBar = () => {
+function RightBar(){
     return (
-        <div className='rightBar'>
-            rightBar
+        <div className = "rightBar">
+            <div className='container'>
+                
+
+                <div className='item'>
+                    <span>Latest Activities</span>
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <p>
+                                <span>TMK </span>    
+                                Changed their cover picture
+                            </p>
+                        </div>
+
+                        <span>1 min ago</span>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <p>
+                                <span>TMK </span>    
+                                Changed their cover picture
+                            </p>
+                        </div>
+
+                        <span>1 min ago</span>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <p>
+                                <span>TMK </span>    
+                                Changed their cover picture
+                            </p>
+                        </div>
+
+                        <span>1 min ago</span>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <p>
+                                <span>TMK </span>    
+                                Changed their cover picture
+                            </p>
+                        </div>
+
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className='item'>
+                    <span>Online Friends</span>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <div className='online'></div>
+                            <span>TMK</span>   
+                        </div>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <div className='online'></div>
+                            <span>TMK</span>   
+                        </div>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <div className='online'></div>
+                            <span>TMK</span>   
+                        </div>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <div className='online'></div>
+                            <span>TMK</span>   
+                        </div>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <div className='online'></div>
+                            <span>TMK</span>   
+                        </div>
+                    </div>
+
+                    <div className='user'>
+                        <div className='userInfo'>
+                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
+                            alt=''/>
+                            <div className='online'></div>
+                            <span>TMK</span>   
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
 
-export default RightBar
+export default RightBar;
