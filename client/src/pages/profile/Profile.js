dfc34599e26aa87a5d5f0594a624664912dced5c
NhutThi2404 <nhutth2404@gmail.com>
2023-12-30 21:08:51 +0700

    add upload image (not complete)

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -8,16 +8,38 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import EmailOutlined from '@mui/icons-material/EmailOutlined';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 import Post from '../../components/post/Post';
 import Posts from '../../components/posts/Posts';
+
+import { useRef, useState } from 'react';
+
 const Profile = () => {
+    const inputRef = useRef(null);
+    const [image, setImage] = useState("");
+
+
+    const handleImageClick = () =>{
+        inputRef.current.click();
+    }
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        console.log(file);
+        setImage(file);
+    }
+
     return (
         <div className='profile'>
             <div className='images'>
                 <img src="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
                     alt="" className='cover'/>
-                <img src="https://images.pexels.com/photos/2055500/pexels-photo-2055500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                    alt="" className='profilePic'/>
+                <div className='profile-image-container' onClick={handleImageClick}> 
+                    {image ? <img src={URL.createObjectURL(image)} alt='' className='profile-picture-after'/> : <img src='https://images.pexels.com/photos/2055500/pexels-photo-2055500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='profile-picture-before'/>}
+                    <FileUploadIcon style={{fontSize: 50}} className='upload-image-icon'/>
+                    <input type = "file" ref = {inputRef} onChange={handleImageChange} className='upload-image-btn'/>
+                </div>
+                
             </div>
 
             <div className='profileContainer'>
