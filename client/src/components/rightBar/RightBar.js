7add863befcbb43335df680b09c4bacab0624ba1
Tran Minh Khanh <kidopro03@gmail.com>
2024-01-01 21:23:44 +0700

    update rightbar

--- client/src/components/rightBar/RightBar.js
+++ client/src/components/rightBar/RightBar.js
@@ -15,46 +15,6 @@ function RightBar(){
                         </div>
                     </div>
 
-                    <div className='user'>
-                        <div className='userInfo'>
-                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                            alt=''/>
-                            <span>TMK</span>   
-                        </div>
-                    </div>
-
-                    <div className='user'>
-                        <div className='userInfo'>
-                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                            alt=''/>
-                            <span>TMK</span>   
-                        </div>
-                    </div>
-
-                    <div className='user'>
-                        <div className='userInfo'>
-                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                            alt=''/>
-                            <span>TMK</span>   
-                        </div>
-                    </div>
-
-                    <div className='user'>
-                        <div className='userInfo'>
-                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                            alt=''/>
-                            <span>TMK</span>   
-                        </div>
-                    </div>
-
-                    <div className='user'>
-                        <div className='userInfo'>
-                            <img src='https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' 
-                            alt=''/>
-                            <span>TMK</span>   
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
