a7c2059496bc43b9224faabf118e52d132f13f9f
Tran Minh Khanh <kidopro03@gmail.com>
2024-01-03 19:37:52 +0700

    fetch comments (cần thêm date, profile pic, username comment)

--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -10,52 +10,52 @@ const Comments = ({ postId }) => {
     const {currentUser} = useContext(AuthContext)
     // console.log(postId);
     // Queries
-    // const {
-    //     isLoading,
-    //     error,
-    //     data: comments,
-    // } = useQuery({
-    //     queryKey: ["posts"],
-    //     queryFn: () => {
-    //     try {
-    //         return axios
-    //         .get(`http://localhost:3001/posts/comments/${postId}`)
-    //         .then((response) => {
-    //             // console.log(response.data);
-    //             return response.data;
-    //         });
-    //     } catch (error) {
-    //         // console.error(error);
-    //         throw error; // Re-throw to allow useQuery to handle error
-    //     }
-    //     },
-    // });
+    const {
+        isLoading,
+        error,
+        data: Comments,
+    } = useQuery({
+        queryKey: ["comments"],
+        queryFn: () => {
+        try {
+            return axios
+            .get(`http://localhost:3001/posts/comments/${postId}`)
+            .then((response) => {
+                // console.log(response.data);
+                return response.data;
+            });
+        } catch (error) {
+            // console.error(error);
+            throw error; // Re-throw to allow useQuery to handle error
+        }
+        },
+    });
 
-    // if (isLoading) {
-    //     return <h3>Loading...</h3>;
-    // }
+    if (isLoading) {
+        return <h3>Loading...</h3>;
+    }
 
-    // if (error) {
-    //     return <h3>Error: {error.message}</h3>;
-    // }
+    if (error) {
+        return <h3>Error: {error.message}</h3>;
+    }
     
-    const comments = [
-        {
-            id: 1,
-            desc: "Hello world Hello world Hello world Hello world Hello world Hello world",
-            name: "John Doe",
-            userId: 1,
-            profilePic: "https://images.pexels.com/photos/2055500/pexels-photo-2055500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        },
-        {
-            id: 2,
-            desc: "Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK",
-            name: "John Doe",
-            userId: 2,
-            profilePic: "https://images.pexels.com/photos/2055500/pexels-photo-2055500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        },
+    // const comments = [
+    //     {
+    //         id: 1,
+    //         desc: "Hello world Hello world Hello world Hello world Hello world Hello world",
+    //         name: "John Doe",
+    //         userId: 1,
+    //         profilePic: "https://images.pexels.com/photos/2055500/pexels-photo-2055500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    //     },
+    //     {
+    //         id: 2,
+    //         desc: "Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK Hello TMK",
+    //         name: "John Doe",
+    //         userId: 2,
+    //         profilePic: "https://images.pexels.com/photos/2055500/pexels-photo-2055500.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    //     },
 
-    ]
+    // ]
     return (
         <div className='comments'>
             <div className='write'>
@@ -63,12 +63,12 @@ const Comments = ({ postId }) => {
                 <input type='text' placeholder='Write a comment'/>
                 <button>Send</button>
             </div>
-            {comments.map((comment) => {
-                return <div className='comment' key={comment.id}>
-                    <img src={comment.profilePic} alt=""/>
+            {Comments.comments.map((comment) => {
+                return <div className='comment'>
+                    <img src="https://images.pexels.com/photos/2783848/pexels-photo-2783848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
                     <div className='info'>
-                        <span>{comment.name}</span>
-                        <p>{comment.desc}</p>
+                        <span>TMK</span>
+                        <p>{comment.content}</p>
                     </div>
                     <span className='date'>1 hour ago</span>
                 </div>
