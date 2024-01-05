0ef890d8ced2160c2cfaf25f5207576f94be7930
ThanhPhuc18 <vtphuc21@apcs.fitus.edu.vn>
2024-01-05 22:21:45 +0700

    Update moderator.js

--- server/functions/moderator.js
+++ server/functions/moderator.js
@@ -14,10 +14,10 @@ async function removePost(currentUser, postId) {
     if (post) {
       await post.deletePost();
     } else {
-      console.error(`Post with ID ${postId} not found`);
+      console.error(`Post with ID ${postId} not found!`);
     }
 
-    console.log(`Post with ID ${postId} removed successfully.`);
+    console.log(`Post with ID ${postId} removed successfully!`);
   } catch (error) {
     console.error("Error removing post:", error.message);
   }
