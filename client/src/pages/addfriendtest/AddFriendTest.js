f83db533653333eedbce83235a0cddaf5ea9b101
NhutThi2404 <nhutth2404@gmail.com>
2024-01-07 02:44:15 +0700

    edit file addfriendtest

--- client/src/pages/addfriendtest/AddFriendTest.js
+++ client/src/pages/addfriendtest/AddFriendTest.js
@@ -5,34 +5,9 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 const AddFriendTest = () => {
     return (
-        <div className='add-friend-page'>
-            <div className='image'>
-                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='cover-image' />
-                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='profile-image' />
-            </div>
+        <div className='add-friend-page-test'>
             <div className='profile-container'>
                 <div className='user-info'>
-
-                    <div className='center'>
-                        <div className='name'>
-                            <span>Current User name</span>
-                        </div>
-
-                        <div className='social-link-friend'>
-                            <a href='https://facebook.com'>
-                                <FacebookIcon style={{ fontSize: 30 }} className='logo' />
-                            </a>
-
-                            <a href='https://instagram.com'>
-                                <InstagramIcon style={{ fontSize: 30 }} className='logo' />
-                            </a>
-
-                            <a href='https://www.linkedin.com/'>
-                                <LinkedInIcon style={{ fontSize: 30 }} className='logo' />
-                            </a>
-                        </div>
-
-                    </div>
 
                     <div className='invitation'>
                         <div className='get-link'>
