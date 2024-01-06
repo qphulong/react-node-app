14f08ab7015a40319ad594612bdbba2a5b9859f7
NhutThi2404 <nhutth2404@gmail.com>
2024-01-07 02:12:26 +0700

    edit temp page add friends test

--- client/src/pages/addfriendtest/AddFriendTest.js
+++ client/src/pages/addfriendtest/AddFriendTest.js
@@ -1,8 +1,80 @@
-
+import './addFriendTest.scss'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 const AddFriendTest = () => {
     return (
-        <div>
-            This is test page
+        <div className='add-friend-page'>
+            <div className='image'>
+                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='cover-image' />
+                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='profile-image' />
+            </div>
+            <div className='profile-container'>
+                <div className='user-info'>
+
+                    <div className='center'>
+                        <div className='name'>
+                            <span>Current User name</span>
+                        </div>
+
+                        <div className='social-link-friend'>
+                            <a href='https://facebook.com'>
+                                <FacebookIcon style={{ fontSize: 30 }} className='logo' />
+                            </a>
+
+                            <a href='https://instagram.com'>
+                                <InstagramIcon style={{ fontSize: 30 }} className='logo' />
+                            </a>
+
+                            <a href='https://www.linkedin.com/'>
+                                <LinkedInIcon style={{ fontSize: 30 }} className='logo' />
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div className='invitation'>
+                        <div className='get-link'>
+                            <p>Give this link to someone you want to add</p>
+                            <div className='random-link-container'>
+                                <span>Here is the link</span>
+                                <div>
+                                    <ContentCopyIcon style={{ fontSize: 30 }} className='logo' />
+                                    <div className='tool-tip'>Copy link</div>
+                                </div>
+                            </div>
+
+                            <button className='get-link-button' >Generate link</button>
+                        </div>
+                        <div className='get-password'>
+                            <p>Password to add</p>
+                            <div className='password-container'>
+                                <div className='password'>
+                                    <input
+                                        type='text'
+                                        id='password'
+                                        placeholder='Your password'
+                                    />
+                                </div>
+
+                                <div className='copy-password-icon'>
+                                    <ContentCopyIcon style={{ fontSize: 30 }} className='logo' />
+                                    <div className='tool-tip'>
+                                        Copy password
+                                    </div>
+                                </div>
+
+                            </div>
+                            <button className='change-password-button'>
+                                Save
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
