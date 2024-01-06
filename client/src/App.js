dd725c00dfe4e616fee02942d83ad098dcd753f9
Tran Minh Khanh <kidopro03@gmail.com>
2024-01-06 10:43:13 +0700

    setup change page

--- client/src/App.js
+++ client/src/App.js
@@ -94,7 +94,7 @@ function App() {
           element: <AddFriend/>
         },
         {
-          path: "/changepassword",
+          path: "/change",
           element: <ChangePassword/>
         },
         // {
