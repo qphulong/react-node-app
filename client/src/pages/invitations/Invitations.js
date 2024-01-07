92edad5c373e9969276e9e2cbb08dcf1e40875ef
Tran Minh Khanh <kidopro03@gmail.com>
2024-01-07 14:19:01 +0700

    Update logic invitations

--- client/src/pages/invitations/Invitations.js
+++ client/src/pages/invitations/Invitations.js
@@ -1,13 +1,15 @@
 import './invitations.scss'
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import Button from '@mui/material/Button';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { AuthContext } from '../../context/authContext';
+import axios from 'axios';
 
 const Invitations = () => {
 
     const { currentUser} = useContext(AuthContext);
+    const [isFriendOrNot,setIsFriendOrNot] = useState(false)
 
     // const data = "add-friends/tmk3010-e5eaee39-819d-45b8-991f-de9a997fb4b4"
     // const extractedValue = data.split('/')[1].split('-')[0]; // "tmk3010"
@@ -15,10 +17,54 @@ const Invitations = () => {
     const {link} = useParams()
     const userId = link.split('-')[0];
 
-    console.log('====================================');
-    console.log(link);
-    console.log(userId);
-    console.log('====================================');
+    // console.log('====================================');
+    // console.log(link);
+    // console.log(userId);
+    // console.log('====================================');
+
+    const checkIsFriend = async () => {
+        try {
+            const response = await axios.get(`http://localhost:3001/user/friends/check/${currentUser.userId}/${userId}`);
+        
+            if (response.status === 200) {
+                // console.log('====================================');
+                // console.log("Is friend: ",response.data.isFriend);
+                // console.log('====================================');
+                setIsFriendOrNot(response.data.isFriend)
+            } 
+            else {
+                console.log(`Unexpected response: ${JSON.stringify(response.data)}`);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    };
+
+    useEffect(() => {
+        checkIsFriend()
+    },[])
+
+     // if(userId === currentUser.userId){
+    //     return (
+    //         <div>
+    //             You are currentuser so you can't access this link
+    //         </div>
+    //     )
+    // }
+
+    // useEffect(() => {
+    //     console.log('====================================');
+    //     console.log(isFriendOrNot);
+    //     console.log('====================================');
+    // },[isFriendOrNot])
+
+    // if(isFriendOrNot === true){
+    //     return (
+    //         <div>
+    //             You are already friend with {userId}
+    //         </div>
+    //     )
+    // }
 
 
     return (
