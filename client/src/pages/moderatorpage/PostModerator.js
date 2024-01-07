8bfdffc9419e769911023f9939e42617ae5fa970
NhutThi2404 <nhutth2404@gmail.com>
2024-01-07 21:47:41 +0700

    update slider images in moderator

--- client/src/pages/moderatorpage/PostModerator.js
+++ client/src/pages/moderatorpage/PostModerator.js
@@ -1,10 +1,26 @@
-import  './postModerator.scss';
+import './postModerator.scss';
+import { useEffect, useState, useRef, useContext } from "react";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 const PostModerator = () => {
+    
+    const [images, setImages] = useState(['https://pbs.twimg.com/media/GCfJUsAaEAATOon?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/GCfJLAEaMAAXvmP?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/GCfHyvCaUAAODgW?format=jpg&name=4096x4096']);
+
+    const [current, setCurrent] = useState(0);
+    const length = images.length;
+
+    const nextSlide = () => {
+        setCurrent((prevCurrent) => (prevCurrent === length - 1 ? 0 : prevCurrent + 1));
+    };
+    
+    const prevSlide = () => {
+        setCurrent((prevCurrent) => (prevCurrent === 0 ? length - 1 : prevCurrent - 1));
+    };
+
     return (
         <div className='post-moderator'>
             <div className='top-part'>
-                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='profile-picture'/>
+                <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='profile-picture' />
                 <div className='post-info'>
                     <span className='user-name'>Hong Nhut</span>
                     <span className='time'>1 min ago</span>
@@ -14,16 +30,24 @@ const PostModerator = () => {
                 <div className='content'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
                 </div>
-                <div className='picture-list'>
-                    <img src='https://pbs.twimg.com/media/GCfJUsAaEAATOon?format=jpg&name=4096x4096' alt='' className='
-                    picture'/>
-                    <img src='https://pbs.twimg.com/media/GCfJLAEaMAAXvmP?format=jpg&name=4096x4096' alt='' className='
-                    picture'/>
-                    <img src='https://pbs.twimg.com/media/GCfJLAEaMAAXvmP?format=jpg&name=4096x4096' alt='' className='
-                    picture'/>  
-                    <img src='https://pbs.twimg.com/media/GCfJUsAaEAATOon?format=jpg&name=4096x4096' alt='' className='
-                    picture'/>    
-                </div>
+                {images &&
+                    <section className='slider'>
+                        {images.map((image, index) => {
+                            return (
+                                <div
+                                    className={index === current ? 'slide active' : 'slide'}
+                                    key={index}
+                                >
+                                    <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+                                    <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+                                    {index === current && (
+                                        <img src={image} alt='travel image' className='image' />
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </section>}
+
             </div>
             <div className='bottom-part'>
                 <button className='accept-btn'>Accept</button>
