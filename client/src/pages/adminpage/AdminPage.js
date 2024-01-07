694fad3b5caa89d7ed77a37b6afb99cfb882cbbd
Tran Minh Khanh <kidopro03@gmail.com>
2024-01-08 00:14:01 +0700

    Update UI + setup

--- client/src/pages/adminpage/AdminPage.js
+++ client/src/pages/adminpage/AdminPage.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/authContext';
 import axios from 'axios';
+import UserElement from './UserElement';
 const AdminPage = () => {
     const { currentUser, login, logout, profileImage } = useContext(AuthContext);
     const navigate = useNavigate()
@@ -92,13 +93,9 @@ const AdminPage = () => {
                 </div>
             </div>
             <div className='list-user'>
-                <div className='user'>
-                    <div className='user-info'>
-                        <img src='https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' className='user-picture' />
-                        <span className='user-name'>Username</span>
-                    </div>
-                    <button className='set-moderator-button'>Set Moderator</button>
-                </div>
+                {allUsers?.map((user) => {
+                    return <UserElement userIdElement = {user} key={user} />;
+                })}
             </div>
         </div>
 
