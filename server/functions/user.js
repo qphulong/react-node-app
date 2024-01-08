a2de804b56944fc1d8fed2b72fca3df3d5ccd7c9
viettungvuong <viettungvuong@gmail.com>
2024-01-08 14:54:06 +0700

    bcryptjs

--- server/functions/user.js
+++ server/functions/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const uuid = require("uuid");
 
 async function signIn(currentUser, userId, inputPassword, res) {
