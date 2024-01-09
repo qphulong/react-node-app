e7ec281c8fdefcafeb8dd392b8a7cb2d7d302ef3
qplong21 <qphulong@gmail.com>
2024-01-09 22:06:28 +0700

    small bug fixed

--- server/functions/admin.js
+++ server/functions/admin.js
@@ -3,38 +3,32 @@ const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
 async function assignModerator(userId, res) {
-  const user = await User.findOne({ userId: userId });
+    const user = await User.findOne({ userId: userId });
 
-  if (!user) {
-    res.status(400).send("User not found!");
-    return;
-  }
+    if (!user) {
+        res.status(400).send("User not found!");
+        return;
+    }
 
-  await User.updateOne(
-    { userId: userId },
-    { $set: { isContentModerator: true } }
-  );
+    await User.updateOne({ userId: userId }, { $set: { isContentModerator: true } });
 
-  res.send("User assigned as a moderator successfully!");
+    res.send("User assigned as a moderator successfully!");
 }
 
 async function unassignModerator(userId, res) {
-  const user = await User.findOne({ userId: userId });
+    const user = await User.findOne({ userId: userId });
 
-  if (!user) {
-    res.status(400).send("User not found!");
-    return;
-  }
+    if (!user) {
+        res.status(400).send("User not found!");
+        return;
+    }
 
-  await User.updateOne(
-    { userId: userId },
-    { $set: { isContentModerator: false } }
-  );
+    await User.updateOne({ userId: userId }, { $set: { isContentModerator: false } });
 
-  res.send("User unassigned as a moderator successfully!");
+    res.send("User unassigned as a moderator successfully!");
 }
 
 module.exports = {
-  assignModerator,
-  unassignModerator,
+    assignModerator,
+    unassignModerator,
 };
